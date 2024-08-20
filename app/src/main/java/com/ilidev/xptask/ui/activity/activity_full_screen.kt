55b3d99d25e9990b9ec3e674858f50d54dd263ba
iliaxp <cafeelectro596@gmail.com>
2024-08-20 09:37:38 +0330

    one

---
+++ app/src/main/java/com/ilidev/xptask/ui/activity/activity_full_screen.kt
@@ -0,0 +1,21 @@
+package com.ilidev.xptask.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ilidev.xptask.R
+
+class activity_full_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_full_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
